af55803fcb8adb15eea17d9f84d91d4deef6dad1
Giuseppe <giuseppegiovanni.amoruso@gmail.com>
2019-09-14 19:19:51 +0200

    add scroll event

---
+++ script/script.js
@@ -0,0 +1,16 @@
+const nav = document.querySelector('nav');
+const jumbo = document.querySelector('main');
+const classScroll = 'scrollable';
+
+const navOffset = nav.offsetTop;
+const addScroolEv= () => {
+    if(window.pageYOffset > navOffset) {
+        nav.classList.add(classScroll);
+        jumbo.style.paddingTop('102px');
+    }
+    else {
+        nav.classList.remove(classScroll);
+    }
+}
+
+window.addEventListener("scroll", addScroolEv);
