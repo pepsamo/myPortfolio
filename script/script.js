5780815deaa5c96c5ddb9d3f96c3c69c56e56c8d
Giuseppe <giuseppegiovanni.amoruso@gmail.com>
2019-09-19 01:50:02 +0200

    Change icon when button is clicked

--- script/script.js
+++ script/script.js
@@ -1,16 +1,37 @@
-const nav = document.querySelector('nav');
-const jumbo = document.querySelector('main');
-const classScroll = 'scrollable';
+// const nav = document.querySelector('nav');
+// const jumbo = document.querySelector('main');
+// const classScroll = 'scrollable';
 
-const navOffset = nav.offsetTop;
-const addScroolEv= () => {
-    if(window.pageYOffset > navOffset) {
-        nav.classList.add(classScroll);
-        jumbo.style.paddingTop('102px');
+// const navOffset = nav.offsetTop;
+// const addScroolEv= () => {
+//     if(window.pageYOffset > navOffset) {
+//         nav.classList.add(classScroll);
+//         jumbo.style.paddingTop('102px');
+//     }
+//     else {
+//         nav.classList.remove(classScroll);
+//     }
+// }
+
+// window.addEventListener("scroll", addScroolEv);
+
+//Add close button when the menu burger is clicked
+const closed =  "fa-times";
+const burger = "fa-bars";
+
+const $menu = document.querySelector("#menu");
+const icon = document.querySelector(".fas");
+
+const changeButton = (element) => {
+    let btnClass = element.target.className; 
+    if(btnClass.includes(burger)) {
+        icon.classList.toggle(burger);
+        icon.classList.toggle(closed);
+        console.log(btnClass);
     }
     else {
-        nav.classList.remove(classScroll);
+        icon.classList.toggle(closed);
+        icon.classList.toggle(burger);
     }
 }
-
-window.addEventListener("scroll", addScroolEv);
+$menu.addEventListener("click", changeButton);
