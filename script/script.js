4eaaf397634c9131ee6a752159c0c2585df63124
Giuseppe <giuseppegiovanni.amoruso@gmail.com>
2019-09-23 18:07:18 +0200

    Change the text of the menu button

--- script/script.js
+++ script/script.js
@@ -16,22 +16,3 @@
 // window.addEventListener("scroll", addScroolEv);
 
 //Add close button when the menu burger is clicked
-const closed =  "fa-times";
-const burger = "fa-bars";
-
-const $menu = document.querySelector("#menu");
-const icon = document.querySelector(".fas");
-
-const changeButton = (element) => {
-    let btnClass = element.target.className; 
-    if(btnClass.includes(burger)) {
-        icon.classList.toggle(burger);
-        icon.classList.toggle(closed);
-        console.log(btnClass);
-    }
-    else {
-        icon.classList.toggle(closed);
-        icon.classList.toggle(burger);
-    }
-}
-$menu.addEventListener("click", changeButton);
